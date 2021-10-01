d3fc58ce0d56ed7f5e3067b90d192d7913cfb3f1
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-10-01 11:25:48 +0530

    Create TA3_Solution.py

---
+++ TA3_Solution.py
@@ -0,0 +1,38 @@
+import pygame
+pygame.init() 
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+size=(600,600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+paddle=pygame.Rect(300,500,60,10)
+ball=pygame.Rect(200,250,10,10)
+ballx=-1
+bally=-1
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                  carryOn = False             
+    screen.fill(DARKBLUE)
+    
+    pygame.draw.rect(screen,LIGHTBLUE,paddle)
+    ball.x=ball.x+ballx
+    ball.y=ball.y+bally
+    if ball.x>=590:
+      ballx=-ballx
+    if ball.x<=10:
+      ball.x=-ballx
+      
+    #Boundaries for y-axis 
+    if ball.y>=590:
+      bally=-bally
+    if ball.y<=10:
+      bally=-bally
+      
+    pygame.draw.rect(screen,WHITE ,ball)
+    pygame.time.wait(20)
+    pygame.display.flip()
+pygame.quit()
+    
